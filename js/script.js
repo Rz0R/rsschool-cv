c323007a8d3fdfb38d421a8eeb2a4fc4c39edc60
Rz0R <vpa05@mail.ru>
2021-09-21 10:16:18 +1000

    fix: fix markup

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
-import { displayRaiting } from './raiting.js';
-
 const menuButton = document.querySelector('.menu__button');
 const menuLinks = document.querySelectorAll('.menu__link');
 
-menuButton.addEventListener('click', function() {
-    document.body.classList.toggle('menu__button--open');
+menuButton.addEventListener('click', function () {
+  document.body.classList.toggle('menu__button--open');
 });
 
 for (const menuLink of menuLinks) {
-    menuLink.addEventListener('click', function() {
-        document.body.classList.remove('menu__button--open');
-    });
+  menuLink.addEventListener('click', function () {
+    document.body.classList.remove('menu__button--open');
+  });
 }
-
-// self-raiting
-displayRaiting();
